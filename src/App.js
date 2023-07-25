5c9d7020fe829bf2e455ad0cbdfd139744a5de63
ziayiing <ziayiing86@gmail.com>
2023-07-25 10:03:08 +0800

    added weather js files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 
-import './App.css';
+import React from "react";
+import Weather from "./WeatherSearch";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <div className="container">
+        <Weather defaultCity="Kota Kinabalu" />
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.ziayiing86@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ling Zia Yiing
+          </a>
+          {""} and is {""}
+          open sourced on {""}
+          <a
+            href="https://github.com/ziayiing/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
